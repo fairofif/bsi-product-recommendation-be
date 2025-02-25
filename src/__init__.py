d2709da908c5fdc1d6245f463e5df591039209f9
Damar Buana Murti <damarbuanamurti@gmail.com>
2025-02-25 17:10:59 +0700

    fix: remove print

--- src/__init__.py
+++ src/__init__.py
@@ -9,7 +9,6 @@ load_dotenv()
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-print(os.getenv("DATABASE_URL"))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
